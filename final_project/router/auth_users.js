96f1104c24b4c0b2127049ab7e081476041b05a2
“pragyaGithubVC” <prag11neupane@gmail.com>
2023-08-04 17:18:44 -0400

    tasks 1-9 complete

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,20 +9,63 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+    console.log('users is: '+ users);
+    let validUsers = users.filter((user)=> {
+        return (user.username === username && user.password === password);
+    });
+    if(validUsers.length > 0){
+        return true;
+    }else{ 
+        return false; 
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(`username 2: ${username}, password: ${password}`);
+  if(!username || ! password){
+      return res.status(404).json({message: "Error logging in"});
+  }
+  if(authenticatedUser(username,password)){
+      let accessToken = jwt.sign(
+        {data: password}, 
+        'access',
+        {expiresIn: 60 * 60}
+       );
+     req.session.authorization = {
+         accessToken,username
+     }
+     return res.status(200).send("Customer successfully logged in");
+  }else{
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.authorization['username'];
+    var bookToUpdateReviewIn = books[isbn];
+    console.log('username is: '+ username);
+    bookToUpdateReviewIn.reviews[username] = review;
+    //console.log('bookToUpdateReviewIn : ' +JSON.stringify(bookToUpdateReviewIn, null, 4));
+    return res.status(200).send(`The review for the book with isbn ${isbn} has been added/updated`);
+  });
+  
+//Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) =>{
+    const isbn = req.params.isbn;
+    const username = req.session.authorization['username'];
+    const book = books[isbn];
+    console.log('book : ' +JSON.stringify(book, null, 4));
+    if(book.reviews[username]){
+        delete book.reviews[username];
+    }
+    return res.status(200).send(`Reviews for the ISBN ${isbn} posted by the user ${username} deleted`);
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,76 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username) =>{
+    let usersWithsamename = users.filter((user)=>{
+        return user.username === username;
+    });
+    if(usersWithsamename > 0){
+        return true;
+    }else{
+        return false;
+    }
+}
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  //console.log(`username: ${username}, password: ${password}`);
+  console.log('users in register is:'+ users);
+  if(username && password){
+      console.log(`username 1 : ${username}, password: ${password}`);
+    if(!doesExist(username)){
+        users.push({"username": username, "password": password});
+        return res.status(200).json({message: "Customer successfully registred. Now you can login"})
+    }else{
+        return res.status(404).json({message: "Customer already exists!"})
+    }
+  }
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let filteredBooks;
+  for(var i in books){
+    if(books[i].author === author){
+        var arr = [{"isbn": i, "title": books[i].title, "reviews": books[i].reviews}];
+        filteredBooks = {"booksByAuthor": arr};
+    }
+  }
+  return res.send(JSON.stringify(filteredBooks, null, 4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let filteredBooks;
+    for(var i in books){
+      if(books[i].title === title){
+          var arr = [{"isbn": i, "author": books[i].author, "reviews": books[i].reviews}];
+          filteredBooks = {"booksByTitle": arr};
+      }
+    }
+    console.log(JSON.stringify(filteredBooks, null, 4));
+    return res.send(JSON.stringify(filteredBooks, null, 4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
